56a4e5890fbe4aaf6dbf32765c86ac938b44041b
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-27 17:27:09 +0800

    object added properties / delete properties

--- Objects/script.js
+++ Objects/script.js
@@ -1,21 +1,20 @@
-// Factory Function
-/*
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log('draw');
-    },
-  };
-}
-const circle = createCircle(1);
-*/
-
-// Constructor Function
+//
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
     console.log('draw');
   };
 }
-const circle = new Circle(1);
+
+const circle = new Circle(10);
+
+// adding properties
+circle.location = { x: 1, y: 1 };
+
+// another access properties
+// 持有特殊字符的时候，用这个访问方法比较好
+const propertyName = 'location';
+circle['location'] = { x: 1 };
+
+// delete properties
+delete circle['location'];
