4fa0e84709ffed5617da19d2258fae27d6e042ea
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-27 17:35:37 +0800

    // Enumerating Properties // another Enumerating Properties // 判断这个key 是否存在， use in key

--- Objects/script.js
+++ Objects/script.js
@@ -18,3 +18,18 @@ circle['location'] = { x: 1 };
 
 // delete properties
 delete circle['location'];
+
+// Enumerating Properties
+// for (let key in circle) {
+//   if (typeof circle[key] !== 'function') console.log(key, circle[key]);
+// }
+
+// another Enumerating Properties
+const keys = Object.keys(circle);
+console.log(keys);
+console.log(circle[keys[0]]);
+
+// 判断这个key 是否存在， use in key
+if ('radius' in circle) {
+  console.log('radius key exist');
+}
